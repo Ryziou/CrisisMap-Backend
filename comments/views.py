db75f9366092b9372e45274917662343076b6bd9
CLiu <callumliu09@hotmail.co.uk>
2025-06-04 10:44:33 +0100

    Added author to comments and ordered comments by latest

--- comments/views.py
+++ comments/views.py
@@ -3,11 +3,20 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from lib.permissions import isOwnerOrReadOnly
 from .models import Comment
 from .serializers.common import CommentSerializer
+from rest_framework.exceptions import ValidationError
 
 class CommentListView(ListCreateAPIView):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        event_id = self.request.query_params.get('event')
+        if not event_id:
+            raise ValidationError({ 'event': 'Event ID is required in query parameters'})
+        return Comment.objects.filter(event=event_id).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        return self.save(author=self.request.user)
 
 
 class CommentDetailView(RetrieveUpdateDestroyAPIView):
