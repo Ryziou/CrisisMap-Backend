d4b0c6d0c0f75c0dbc020f5aa2a88ec722f0a51a
CLiu <callumliu09@hotmail.co.uk>
2025-06-04 10:21:52 +0100

    Added isOwnerOrReadOnly perms

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Comment
+from .serializers.common import CommentSerializer
 
-# Create your views here.
+class CommentListView(ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class CommentDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
