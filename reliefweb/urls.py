1d99063aad682000957aec7b40e2674be0981336
CLiu <callumliu09@hotmail.co.uk>
2025-06-08 14:01:48 +0100

    Added multiple reliefweb stats for homepage use

--- reliefweb/urls.py
+++ reliefweb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import reliefweb_disasters
+from .views import reliefweb_disasters, reliefweb_stats
 
 urlpatterns = [
     path('disasters/', reliefweb_disasters),
+    path('stats/', reliefweb_stats)
 ]
