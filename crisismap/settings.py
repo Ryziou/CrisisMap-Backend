908cca56dabf7002c7ec506a519abd6218e27603
CLiu <callumliu09@hotmail.co.uk>
2025-06-11 10:32:18 +0100

    Removed a line

--- crisismap/settings.py
+++ crisismap/settings.py
@@ -93,12 +93,6 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
     'authorization',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:3000',
-    'http://127.0.0.1:5173'
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
